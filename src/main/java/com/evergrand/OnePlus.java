fc70774cac0d8ddfbc1ce587674a5dfbb6b22ce4
shen19950119 <shen19950119@gmail.com>
2020-11-22 17:53:17 +0800

    修改1

--- src/main/java/com/evergrand/OnePlus.java
+++ src/main/java/com/evergrand/OnePlus.java
@@ -7,5 +7,6 @@ package com.evergrand;
 public class OnePlus {
     public static void main(String[] args) {
         System.out.println("i love evergrand,but have to say goodbye!!!");
+        System.out.println("hello world!!");
     }
 }
---
+++ src/main/java/com/evergrand/TwoPlus.java
@@ -0,0 +1,11 @@
+package com.evergrand;
+
+/**
+ * @author 申燕栋
+ * @create 2020-11-22 17:15
+ */
+public class TwoPlus {
+    public static void main(String[] args) {
+        System.out.println("never say goodbye");
+    }
+}
