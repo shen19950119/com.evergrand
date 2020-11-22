b6fbd1bc973d7740773ac59a2aa0da88284ba155
shendong <13214032543@163.com>
2020-11-22 20:46:46 +0800

    修改2

--- src/main/java/com/evergrand/OnePlus.java
+++ src/main/java/com/evergrand/OnePlus.java
@@ -8,7 +8,12 @@ public class OnePlus {
     public static void main(String[] args) {
         System.out.println("i love evergrand,but have to say goodbye!!!");
         System.out.println("hello world!!");
-        System.out.println("hello world!!");
-        System.out.println("hello world!!");
+        System.out.println("hello world4!!");
+        System.out.println("hello world3!!");
+        System.out.println("hello world1!!");
+        System.out.println("hello world!2!");
+
+
+
     }
 }
